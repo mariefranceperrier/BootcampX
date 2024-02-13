8f95f4b844034bbc91d18baf572437789d7a2e33
mariefranceperrier <mariefrance.perrier@gmail.com>
2024-02-13 11:45:45 -0500

    parameterize queries

--- bootcamp_app/teachers.js
+++ bootcamp_app/teachers.js
@@ -7,17 +7,22 @@ const pool = new Pool ({
   database: 'bootcampx'
 })
 
-pool.query(`
+const queryString = `
 SELECT DISTINCT teachers.name as teacher, cohorts.name as cohort
 FROM assistance_requests
 JOIN students ON students.id = student_id
 JOIN cohorts ON cohorts.id = cohort_id
 JOIN teachers ON teachers.id = teacher_id
-WHERE cohorts.name = '${process.argv[2] || 'JUL02'}' 
+WHERE cohorts.name LIKE $1 
 ORDER BY teacher;
-`)
+`;
+
+const cohortName = process.argv[2] || JUL02;
+const values = [`%${cohortName}%`];
+
+pool.query(queryString, values)
   .then(res => {
-    res.rows.forEach(row => {
-      console.log(`${row.cohort}: ${row.teacher}`);
-    })
-  }).catch(err => console.error('query error', err.stack));
+      res.rows.forEach(row => {
+        console.log(`${row.cohort}: ${row.teacher}`);
+      })
+    }).catch(err => console.error('query error', err.stack));
